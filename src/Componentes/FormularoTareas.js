0d6adfe7a719db6ecbaaef66e10cf912e0045015
Felipe Aguilar <felipe.aguilar0g@gmail.com>
2022-11-10 13:36:06 -0600

    Agregando animaciones con

--- src/Componentes/FormularoTareas.js
+++ src/Componentes/FormularoTareas.js
@@ -2,6 +2,7 @@ import React, {useState} from 'react';
 import { v4 as uuidv4 } from 'uuid';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {faPlusSquare} from '@fortawesome/free-solid-svg-icons';
+import {AnimatePresence, motion} from "framer-motion/dist/framer-motion";
 
 const FormularioTareas = ({tareas, cambiarTareas}) => {
 
@@ -28,13 +29,15 @@ const FormularioTareas = ({tareas, cambiarTareas}) => {
                 value={inputTarea}
                 onChange={(e)=> handleInput(e)}
             />
-            <button 
+            <motion.button 
                 type='submit'
                 className='formulario-tareas__btn'
 
+                // Animación
+                whileTap={{scale: 1.3}}
             >
                 <FontAwesomeIcon icon={faPlusSquare} className="formulario-tareas__icono-btn"/>
-            </button>
+            </motion.button>
         </form>
     );
 }
--- src/Componentes/header.js
+++ src/Componentes/header.js
@@ -1,32 +1,51 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {faEyeSlash, faEye} from '@fortawesome/free-solid-svg-icons';
+import {AnimatePresence, motion} from "framer-motion/dist/framer-motion";
+import { isVisible } from '@testing-library/user-event/dist/utils';
 
-const Header = ({mostarCompletadas, cambiarMostrarCompletadas}) => {
+const Header = ({mostarCompletadas, cambiarMostrarCompletadas}) => {// Animación
 
     const toggleCompletadas = () =>{
         cambiarMostrarCompletadas(!mostarCompletadas);
     }
     return ( 
         <header className="header">
-            <h1 className="header__titulo">Lista de Tareas</h1>
-            {mostarCompletadas ? 
-                <button 
-                    className="header__boton"
-                    onClick={()=> toggleCompletadas()}
-                >
-                    No mostrar Completadas
-                    <FontAwesomeIcon icon = {faEyeSlash} className="header__icono-boton"/>
-                </button>
-            :
-                <button 
-                    className="header__boton"
-                    onClick={()=> toggleCompletadas()}
-                >
-                    Mostrar Completadas
-                    <FontAwesomeIcon icon = {faEye} className="header__icono-boton"/>
-                </button>
-            }
+            <motion.h1 
+                className="header__titulo"
+                initial={{ opacity: 0, scale: 0.5 }}
+                animate={{ opacity: 1, scale: 1 }}
+                transition={{ duration: .6 }}
+            >
+                Lista de Tareas
+            </motion.h1>
+            <AnimatePresence>
+                {mostarCompletadas ?
+
+                        <motion.button 
+                            className="header__boton"
+                            onClick={()=> toggleCompletadas()}
+
+                            // Animaciones
+                            whileHover={{ scale: 1.05 }}
+                        >
+                            No mostrar Completadas
+                            <FontAwesomeIcon icon = {faEyeSlash} className="header__icono-boton"/>
+                        </motion.button>
+                    
+                :
+                    <motion.button 
+                        className="header__boton"
+                        onClick={()=> toggleCompletadas()}
+
+                        // Animaciones
+                        whileHover={{ scale: 1.05 }}
+                    >
+                        Mostrar Completadas
+                        <FontAwesomeIcon icon = {faEye} className="header__icono-boton"/>
+                    </motion.button>
+                }
+            </AnimatePresence>
         </header>
     );
 }
